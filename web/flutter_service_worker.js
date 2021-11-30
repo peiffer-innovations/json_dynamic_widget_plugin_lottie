90223b30aa9daceebd92d38ddc03a8524e5a6ce4
root <noop@github.com>
2021-11-30 23:38:56 +0000

    Pages 4

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,22 +3,22 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "d785572b763caa4994091db7159a72a3",
-"/": "d785572b763caa4994091db7159a72a3",
-"manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
+  "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "version.json": "4b6db237b3514a88107a422469adfb0f",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "8d1c0a4fabd283006aa9863366d60b5e",
-"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"assets/assets/pages/lotties.json": "2ac58250992b6d157b4116332ef96a73",
-"assets/assets/lotties/tent.json": "a9f0e9f7c921e176f66304822542ec69",
-"assets/assets/lotties/fireworks.json": "ec044d733ba18c777ff1c7495c565e3b",
-"assets/assets/lotties/bluetoothscanning.json": "9dc51300adb74b8f51fc5b5a6df88613",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.js": "95096bcbd08dc64597b87934707be3f4",
+"index.html": "94305bc1df8d0a30b84fb9876dcd5c37",
+"/": "94305bc1df8d0a30b84fb9876dcd5c37",
+"assets/NOTICES": "6b0e722b95a20fd916471f5aa3b28262",
 "assets/AssetManifest.json": "373e7e4bc7c1439bb5360cc0c5816691",
-"main.dart.js": "85ac48777f979dfddba589a2f70511a1"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+"assets/assets/pages/lotties.json": "071eac2ab8a464dfc1b7b3f60e4643e8",
+"assets/assets/lotties/fireworks.json": "ec044d733ba18c777ff1c7495c565e3b",
+"assets/assets/lotties/tent.json": "a9f0e9f7c921e176f66304822542ec69",
+"assets/assets/lotties/bluetoothscanning.json": "9dc51300adb74b8f51fc5b5a6df88613"
 };
 
 // The application shell files that are downloaded before a service worker can
