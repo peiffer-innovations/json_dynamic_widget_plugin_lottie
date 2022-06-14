4f19e3d2909996168fb50d8d819f0dab7ff9c854
root <noop@github.com>
2022-06-14 03:57:24 +0000

    Pages 26

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "6e2908e034a611d04a01c38b1c98a688",
-"/": "6e2908e034a611d04a01c38b1c98a688",
+"index.html": "81d98beb2a734f90e35e8c5332671965",
+"/": "81d98beb2a734f90e35e8c5332671965",
 "main.dart.js": "9abfb15cc15abc3cf2ad012a0e838766",
 "version.json": "ff966ab969ba381b900e61629bfb9789",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
